57fec23620c858e73e8d8d24274b520824a445a1
Almaas <127813180+it22172778@users.noreply.github.com>
2024-05-12 16:30:30 +0530

    some files deleted

--- app/src/main/java/com/example/notesmad4/MainActivity.kt
+++ app/src/main/java/com/example/notesmad4/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.notesmad4
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.notesmad4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var db : NotesDatabaseHelper
+    private lateinit var notesAdapter: NotesAdapter
 
 
 
@@ -15,6 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        db = NotesDatabaseHelper(this)
+        notesAdapter = NotesAdapter(db.getAllNotes(),this)
+
+        binding.notesRecycleView.layoutManager = LinearLayoutManager(this)
+        
 
         binding.addButton.setOnClickListener{
             val intent = Intent(this,AddNote::class.java)
