f110bd75a33e19200bb629832dd612f54a9d6c2b
Almaas <127813180+it22172778@users.noreply.github.com>
2024-05-12 13:05:49 +0530

    create getAllNotesfunction in NoteDatabaseHelper.kt

--- app/src/main/java/com/example/notesmad4/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/notesmad4/NotesDatabaseHelper.kt
@@ -1,5 +1,6 @@
 package com.example.notesmad4
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
@@ -37,6 +38,35 @@ class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
         db.insert(TABLE_NAME,null,values)
         db.close()
     }
+
+
+    fun getAllNotes(): List<Note> {
+        val noteList = mutableListOf<Note>()
+        val db = readableDatabase
+        val query = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(query,null)
+
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID))
+            val title = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_TITLE))
+            val content = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_CONTENT))
+
+            val note = Note(id, title, content)
+            noteList.add(note)
+
+
+        }
+        cursor.close()
+        db.close()
+        return noteList
+
+
+    }
+
+
+
+
+
 
 
 }
