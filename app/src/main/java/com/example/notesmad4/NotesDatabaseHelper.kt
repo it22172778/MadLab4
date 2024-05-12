a9b11ebedbf9b99220c940272f715f0c651abbc3
Almaas <127813180+it22172778@users.noreply.github.com>
2024-05-12 21:51:40 +0530

    deleNote function is created on NoteDatabaseHelper.kt

--- app/src/main/java/com/example/notesmad4/NotesDatabaseHelper.kt
+++ app/src/main/java/com/example/notesmad4/NotesDatabaseHelper.kt
@@ -90,6 +90,14 @@ class NotesDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE
 
     }
 
+    fun deleteNote(noteId: Int){
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME,whereClause,whereArgs)
+        db.close()
+    }
+
 
 
 
