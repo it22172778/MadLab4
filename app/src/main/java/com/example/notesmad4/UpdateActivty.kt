aa9f6dc56882354213f31edd9f27e5bb33379706
Almaas <127813180+it22172778@users.noreply.github.com>
2024-05-12 17:03:53 +0530

    updateActivty created

---
+++ app/src/main/java/com/example/notesmad4/UpdateActivty.kt
@@ -0,0 +1,11 @@
+package com.example.notesmad4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UpdateActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_activty)
+    }
+}
