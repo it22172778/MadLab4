05b364ff58a61bd67ece5e4583431c89f30136c3
Almaas <127813180+it22172778@users.noreply.github.com>
2024-05-12 21:31:55 +0530

    update also implemented

--- app/src/main/java/com/example/notesmad4/UpdateNoteActivty.kt
+++ app/src/main/java/com/example/notesmad4/UpdateNoteActivty.kt
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.notesmad4.databinding.ActivityAddNoteBinding
+import com.example.notesmad4.databinding.ActivityUpdateNoteBinding
 
 class UpdateNoteActivty : AppCompatActivity() {
 
-    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var binding: ActivityUpdateNoteBinding
     private lateinit var db: NotesDatabaseHelper
     private var noteId: Int = -1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         db = NotesDatabaseHelper(this)
@@ -24,13 +25,13 @@ class UpdateNoteActivty : AppCompatActivity() {
         }
 
         val note = db.getNoteByID(noteId)
-        binding.titleEditText.setText(note.title) //want to change button id
-        binding.contentEditText.setText(note.content)////want to change button id
+        binding.updateTitleEditText.setText(note.title) //want to change button id
+        binding.updateContentEditText.setText(note.content)////want to change button id
 
 
-        binding.saveButton.setOnClickListener {
-            val newTitle = binding.titleEditText.text.toString()
-            val newContent = binding.contentEditText.text.toString()
+        binding.updateSaveButton.setOnClickListener {
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
             val updateNote = Note(noteId,newTitle,newContent)
             db.updateNote(updateNote)
             finish()
