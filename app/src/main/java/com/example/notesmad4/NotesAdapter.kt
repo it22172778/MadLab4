4b91f5ea42fe1962be6d575292b2f9b9ad488185
Almaas <127813180+it22172778@users.noreply.github.com>
2024-05-12 13:33:27 +0530

    implement NoteViewHolder class and overide functions

--- app/src/main/java/com/example/notesmad4/NotesAdapter.kt
+++ app/src/main/java/com/example/notesmad4/NotesAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.notesmad4
 
-class NotesAdapter {
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter (private var note: List<Note>, context: Context):
+    RecyclerView.Adapter<NotesAdapter.NoteViewHolder>() {
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+        val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
